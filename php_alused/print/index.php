6f2dd3b1d1c4e3496ebc739c672ee90261d13720
MaldisLaur <maldis-sveno.laur@khk.ee>
2019-02-13 13:04:45 +0200

    valgusfoori tegemine

--- php_alused/print/index.php
+++ php_alused/print/index.php
@@ -1,16 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: maldis-sveno.laur
- * Date: 13.02.2019
- * Time: 12:42
- */
-echo 'Hello World!<br>';
-echo 'Hello World!<br>';
-echo 'Hello World!<br>';
-
-echo '<hr>';
-
-echo 'Hello World!<br>';
-echo 'Hello World!<br>';
-echo 'Hello World!<br>';
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <?php
+echo '<div id="punane"></div>';
+echo '<div id="kollane"></div>';
+echo '<div id="roheline"></div>';
+?>
+    </body>
+</html>
