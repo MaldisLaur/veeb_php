f7116ff38e48f77bda41e8c190a3ad9a0524e785
MaldisLaur <maldis-sveno.laur@khk.ee>
2019-02-13 12:44:04 +0200

    Parandus eurodele

---
+++ php_alused/print/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maldis-sveno.laur
+ * Date: 13.02.2019
+ * Time: 12:42
+ */
+echo 'Hello World!<br>';
+echo 'Hello World!<br>';
+echo 'Hello World!<br>';
+
+echo '<hr>';
+
+echo 'Hello World!<br>';
+echo 'Hello World!<br>';
+echo 'Hello World!<br>';
