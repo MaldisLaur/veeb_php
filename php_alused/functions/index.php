33e5deab23182bd7d4177809e99ab26919e67434
MaldisLaur <maldis-sveno.laur@khk.ee>
2019-02-20 14:15:36 +0200

    uuendus2

--- php_alused/functions/index.php
+++ php_alused/functions/index.php
@@ -1,32 +1,5 @@
 <?php
-// defineerime funktsiooni nimega paarsuseKontroll
-
-/**
- * function paarsuseKontroll
- * @param $arv
- * print is $arv even or add
- */
-function kasPaaris($arv){
-    if($arv % 2 == 0){
-        $kontroll = true;
-    } else {
-        $kontroll = false;
-    }
-    return $kontroll;
-}
-
-function kirjeldus($paaris) {
-    if($paaris == true) {
-        echo ' on paaris';
-    } else {
-        echo ' on paaritu';
-    }
-    echo '<br>';
-}
-
-// kutsume funktsioon tööle
-for($arv = 0; $arv <= 10; $arv++){
-    echo $arv;
-    $kontroll = kasPaaris($arv);
-    kirjeldus($kontroll);
-}
+require_once 'fnc.php';
+lehePais();
+tabel(15);
+?>
